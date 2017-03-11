b4f65da7d6eb230bfbf2fade66b2e75e2bbb1ad0
thepitagroup <support@thepitagroup.com>
2017-03-11 16:10:01 -0500

    Updates to logic  + Clean up
    Added some comments also removed debug code and changed the logic of authenticating and logign a user.

--- adldap/controllers/AdldapControllers.php
+++ adldap/controllers/AdldapControllers.php
@@ -6,7 +6,7 @@ use Adldap\Adldap;
 class AdldapController extends BaseController
 {
  
- 	protected $allowAnonymous = true;
+ 	protected $allowAnonymous = false;
 
         /**
          * Method to Authenticate user trying to login.
@@ -20,7 +20,6 @@ class AdldapController extends BaseController
             $memberPassword = craft()->request->getRequiredPost('password');
          
             $settings = craft()->plugins->getPlugin('adldap')->getSettings();
- 
             $grphandle = $settings['group'];
         
             $config = [
@@ -38,52 +37,52 @@ class AdldapController extends BaseController
                 "follow_referrals"=>$settings['referrals'],
                     
             ];
-          
+            
+            //LETS START THE LDAP CONECTION
             $ldap = new Adldap();
             $provider = new \Adldap\Connections\Provider($config);
-            $ldap->addProvider('default',$provider);
-        
+            $ldap->addProvider('default',$provider); //TIE IN TO THE DEFAULT  PROVIDER THE CONFIG
+            
             try{
+             
                $ldap->connect('default');
+               //CONNECTING TO THE SERVER AS ADMIN 
                $provider->auth()->bindAsAdministrator();
                $search = $provider->search();
+              
+               
                 try {
-
+                    //AUTHENTICATING THE USER FROM LOGIN PAGE
                     if ($provider->auth()->attempt($memberName, $memberPassword)) {
-
                         $rec = $search->find($memberName);
-
-                        $guid  = $this->_to_p_guid($rec['objectguid'][0]);
+                        //$guid  = $this->_to_p_guid($rec['objectguid'][0]);
 
                         $query = craft()->db->createCommand();
                         $result = $query->select('id')->from('users')->where(['username'=>$memberName])->queryRow();
-                        
-                       // die("MB: ".$memberPassword);
-                        //var_dump($result);exit;
-                        if($result === false){
+                        //DOES USER ALREADY EXIST ON LOCAL CRAFT USER TABLE
+                        if($result === false){ //CREATE USER IN TABL:E
                             
                             $user = new UserModel;
                             $user->username  = $memberName;
                             $user->password  = $memberPassword;
-                            //echo "USERNAME: ".$memberName;
+                        
                             $email =$rec['email'][0];
-                            //var_dump($rec['email'][0]);
+                            
                             if(is_null($rec['email'][0])){
                                 $email = $rec['userprincipalname'][0];
                             }
-                           // echo "EMAIl: ".$email;
+                           
                             $user->email = $email;
                             $fname = $rec['givenname'][0];
                             if( is_null($rec['givenname'][0]) ){
                                 $fname = $rec['cn'][0];
                             }
-                            //echo"<br />FNAME: ".$fname;
+                            
                             $user->firstName = $fname;
                             $lname = $rec['sn'][0];
                             if(is_null($rec['sn'][0])){
                                 $lname = $rec['cn'][0];
                             }
-                            //echo"<br />LNAME: ".$lname;
                             $user->lastName = $lname;
 
                             $user->archived = 0;
@@ -97,33 +96,37 @@ class AdldapController extends BaseController
                             $user->lastLoginDate =date("Y-m-d H:i:s");
                             $user->dateCreated = date("Y-m-d H:i:s");
                             $user->dateUpdated = date("Y-m-d H:i:s");
+                            
 
-                            try{
-                                $id = craft()->users->saveUser($user);
-                            } catch(\Exception $e){
-                                 throw $e;
-                            }
-                            $q2 = craft()->db->createCommand();
-                            $grp = $q2->select('id')->from('usergroups')->where(['handle'=>$grphandle])->queryRow();
-                            //var_dump($grp);exit;
-                            craft()->userGroups->assignUserToGroups($user->id, $grp['id']);
+                           if(craft()->users->saveUser($user)){//STORE USER INFO INTO THE CRAFT USER TABLE 
+                               
+                               
+                          
+                                $q2 = craft()->db->createCommand();
+                                $grp = $q2->select('id')->from('usergroups')->where(['handle'=>$grphandle])->queryRow();
+                                //var_dump($grp);exit;
+                                craft()->userGroups->assignUserToGroups($user->id, $grp['id']);
 
 
-                            if (craft()->users->activateUser($user))
-                            {
-                                    craft()->userSession->setNotice(Craft::t('Successfully activated the user.'));
-                            }
-                            else
-                            {
-                                    craft()->userSession->setError(Craft::t('There was a problem activating the user.'));
-                            }
-                        }else {
+                                if (craft()->users->activateUser($user))
+                                {
+                                        craft()->userSession->setNotice(Craft::t('Successfully activated the user.'));
+                                }
+                                else
+                                {
+                                        craft()->userSession->setError(Craft::t('There was a problem activating the user.'));
+                                }
+                           } else {
+                               die("USER NOT SAVED");
+                           }
+                        }else { //USER ALREDY EXIST IN LOCAL DB
                             $id = $result['id'];
                             $user = craft()->users->getUserById( $id );
                             $user->newPassword  = $memberPassword; //get password from post and submit it to get hashed 
                             craft()->users->saveUser($user); //save user 
                         }
-                        craft()->userSession->loginByUserId($id,true);//LOGIN AS CRAFT USER CORRESPONDING TO username
+                        //die("ALOMST");
+                        craft()->userSession->loginByUserId($user->id,true);//LOGIN AS CRAFT USER CORRESPONDING TO username
                         $this->redirectToPostedUrl();//REDIRECT TO URL that the login from was called from.
 
                     } else {
@@ -133,17 +136,17 @@ class AdldapController extends BaseController
 
                 } catch (\Adldap\Exceptions\Auth\UsernameRequiredException $e) {
                     // The user didn't supply a username.
-                    //die("User did not provide a username");
+                   die("User did not provide a username");
                     craft()->userSession->setFlash('errorMessage', Craft::t('User did not provide username.'));
                 } catch (\Adldap\Exceptions\Auth\PasswordRequiredException $e) {
                     // The user didn't supply a password.
                     craft()->userSession->setFlash('errorMessage', Craft::t('User did not provide password.'));
-                     //die("User did not provide a password");
+                   die("User did not provide a password");
                 }
 
             } catch (\Adldap\Exceptions\Auth\BindException $e) {
                 craft()->userSession->setFlash('errorMessage', Craft::t('Can\'t bind to LDAP server.'));
-                //die("Can't bind to LDAP server!");
+                die("Can't bind to LDAP server!");
             }
 
         
@@ -157,27 +160,6 @@ class AdldapController extends BaseController
             $this->redirect('?status=logout');
         }
         
-        /**
-         * Method to convert binary guid to string
-         * @param binary $guid form AD
-         * @return string
-         */
-        
-        private function _to_p_guid( $guid )
-        {
-            $hex_guid = unpack( "H*hex", $guid );
-            $hex    = $hex_guid["hex"];
-
-            $hex1   = substr( $hex, -26, 2 ) . substr( $hex, -28, 2 ) . substr( $hex, -30, 2 ) . substr( $hex, -32, 2 );
-            $hex2   = substr( $hex, -22, 2 ) . substr( $hex, -24, 2 );
-            $hex3   = substr( $hex, -18, 2 ) . substr( $hex, -20, 2 );
-            $hex4   = substr( $hex, -16, 4 );
-            $hex5   = substr( $hex, -12, 12 );
-
-            $guid = $hex1 . "-" . $hex2 . "-" . $hex3 . "-" . $hex4 . "-" . $hex5;
-
-            return $guid;
-        }
 
 
 }
